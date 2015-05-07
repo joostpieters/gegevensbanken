6d9a2b801e8935b7012731bb2c599fd2d6e3b5be
vincentvliegen1 <vincent.vliegen@student.kuleuven.be>
2015-05-07 13:03:12 +0200

    phpChart

--- scheepvaart/statistics.php
+++ scheepvaart/statistics.php
@@ -10,6 +10,7 @@
 	require_once( "gb/mapper/StatisticsMapper.php" );
 	$mapper = new gb\mapper\StatisticsMapper();
 	$allShipments = $mapper->findAll();
+	require_once("phpChart_Lite\conf.php");
 ?>
 <form method="post">
     
@@ -65,6 +66,23 @@ if($search=='2'){
 $mapper = new gb\mapper\StatisticsMapper();
 $result = $mapper->getNumberOfOrders();
 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>phpChart - Basic Chart</title>
+</head>
+<body>
+    
+<?php
+$pc = new C_PhpChartX(array(array(11, 9, 5, 12, 14)),'basic_chart');
+$pc->draw();
+?>
+
+</body>
+</html>
+<?php
 	}
 ?>
 <?php
